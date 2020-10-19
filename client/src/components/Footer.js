2f231d67d86f2e97a07a8cc1788e2ea3362bc86f
AggieWB <usmaggie23@gmail.com>
2020-10-19 08:06:47 -0700

    Add Footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return <footer>
+        <div>
+            <a href="https://www.aggiewb.com" rel="noreferrer noopener" target="_blank">Aggie Wheeler Bateman</a> &copy; {new Date().getFullYear()}
+        </div>
+    </footer>;
+}
+
+export default Footer;
