fc8475da9c36fb5ca710519d3345ae17b31f61e2
AggieWB <usmaggie23@gmail.com>
2020-10-19 15:42:40 -0700

    Remove second display to only show current operation

--- client/src/components/Display.js
+++ client/src/components/Display.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Display(props){
     return <div id="input-output">
         <input disabled></input>
-        <input id="display" value={props.currentNum} disabled></input>
+        <input id="display" value={props.operation} disabled></input>
     </div>;
 }
 
