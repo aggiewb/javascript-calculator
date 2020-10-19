30f447f989588e3365e21fc4ae7345ad2387270e
AggieWB <usmaggie23@gmail.com>
2020-10-19 11:38:55 -0700

    Add ids of returned element in Operators and Options used for
    css styling

--- client/src/components/Operators.js
+++ client/src/components/Operators.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Operators(props){
-    return <div>
+    return <div id="operators">
         {Object.entries(props.operators).map(operator => {
             const [key, sign] = operator;
             return <button key={key} id={key}>{sign}</button>
--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Options(props){
-    return <div>
+    return <div id="options">
         {Object.entries(props.options).map(option => {
             const [key, sign] = option;
             return <button key={key} id={key}>{sign}</button>;
