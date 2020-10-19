7f7b23d55f20e7102d5d9aac942b0115eab7e1dd
AggieWB <usmaggie23@gmail.com>
2020-10-19 08:06:19 -0700

    Import Footer component, and add render return contents

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Footer from './components/Footer.js';
 
 class App extends React.Component {
   constructor(){
@@ -7,7 +8,11 @@ class App extends React.Component {
   }
 
   render(){
-
+    return <div>
+      <h1>JavaScript Calculator</h1>
+      <p>Powered by React</p>
+      <Footer />
+    </div>;
   }
 }
 
