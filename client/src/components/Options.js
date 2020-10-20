6d1ac7c0b3fbe5d1680660eac008063053caf74d
AggieWB <usmaggie23@gmail.com>
2020-10-20 09:50:26 -0700

    Add conditional for equals button onClick method

--- client/src/components/Options.js
+++ client/src/components/Options.js
@@ -9,6 +9,8 @@ function Options(props){
                 callback = props.clear;
             } else if(key === 'decimal'){
                 callback = props.concatDecimal;
+            } else if(key === 'equals'){
+                callback = props.equals;
             }
             return <button key={key} id={key} onClick={callback}>{sign}</button>;
         })}
