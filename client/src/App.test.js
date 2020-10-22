e98e95f83369464d60f7c65a72d7c89635fe77c6
AggieWB <usmaggie23@gmail.com>
2020-10-22 07:53:50 -0700

    Add a test calling App class method computeOperation() with
    an operation that contains consecutive operators with last operator as -

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -244,6 +244,16 @@ it('calls App class method computeOperation() with an operation that contains ea
   expect(display.prop('result')).toEqual(EXPECTED_RESULT);
 });
 
+it('calls App class method computeOperation() with an operation that contains consecutive operators with last operator as -', () => {
+  const app = shallow(<App />);
+  const INITIAL_OPERATION = '2/+-1';
+  const EXPECTED_RESULT = 1;
+  app.setState({...EXPECTED_INITIAL_STATE, operation: INITIAL_OPERATION});
+  app.instance().computeOperation();
+  const display = app.find('Display');
+  expect(display.prop('result')).toEqual(EXPECTED_RESULT);
+});
+
 it('calls App class method clear() which sets display properties to initial values', () => {
   const app = shallow(<App />);
   app.instance().clear();
